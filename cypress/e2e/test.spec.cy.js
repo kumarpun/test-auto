0614dd03c92965c393d1e7095604e344c531fd60
tekkon <tekkon@tekkons-MacBook-Pro.local>
2025-09-14 13:51:01 +0545

    add github action

--- cypress/e2e/test.spec.cy.js
+++ cypress/e2e/test.spec.cy.js
@@ -1,6 +1,6 @@
 describe('Test', () => {
   it('Test home page', () => {
-      cy.visit('http://localhost:3000')
-      cy.contains(`Welcome to our beautiful website. We're glad to have you here!`)
+      cy.visit('https://www.cypress.io/')
+      cy.contains(`With Cypress, you can easily create tests for your modern web applications`)
   })
 })
